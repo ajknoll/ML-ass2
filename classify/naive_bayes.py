d617b9390659db590d5a4c0d4aad1621aa247cf7
Avi Knoll <gz.ajknoll@gmail.com>
2011-07-06 17:46:26 +1000

    Added some load/save functions

---
+++ classify/naive_bayes.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf_8 -*-
+
+""" 
+Attempts to learn the classification states (baseline, 1, 2, 3) for our task
+using a naive Bayes classifier.
+"""
+
+from data.convertMat import ExperimentData, TaskRecording
