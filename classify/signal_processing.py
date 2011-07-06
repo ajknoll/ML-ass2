eb372118363b1c5bc5e55bbaccd3b27ccace349f
Avi Knoll <gz.ajknoll@gmail.com>
2011-07-07 00:22:08 +1000

    broken rms function included

--- classify/signal_processing.py
+++ classify/signal_processing.py
@@ -11,16 +11,36 @@ import numpy
 class SignalLearn:
 
    def getRmsList(self, data):
+      """
+      Given an ExperimentData object data,
+      produces a list of root mean square (RMS) values per epoch. The feature
+      dimension is a list of signal channels. There is a corresponding list of
+      class labels for the RMS values.
+
+      Returns:
+      sample   - a list of [epoch, channel] RMS values
+      classes  - corresponding list of class labels
+      """
       assert(data.__class__ == ExperimentData)
+      sample = []
+      classes = []
       for subject in data.matrix:
          for task in subject:
             if task.nEpochs == 1:
-               rmsList = [rootMeanSquare(channel) for channel in task.data]
+               rmsList = [self.rootMeanSquare(channel) for channel in task.data]
                sample.append(rmsList)
                classes.append(task.condition)
             else:
                for epoch in seq(task.nEpochs):
-                  rmsList = [rootMeanSquare(channel) for channel in task.data[:, :, epoch]]
+                  rmsList = [self.rootMeanSquare(channel) for channel in task.data[:, :, epoch]]
                   sample.append(rmsList)
                   classes.append(task.condition)
+      return sample, classes
 
+   def rootMeanSquare(self, arrayLike):
+      """
+      Returns the root mean square of the elements in arrayLike.
+      """
+      array = numpy.asanyarray(arrayLike)
+      rms = numpy.sqrt(numpy.sum([numpy.square(elem) for elem in array]))
+      return rms
